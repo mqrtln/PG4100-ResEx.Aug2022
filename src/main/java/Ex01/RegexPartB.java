9a231e7050ca58430496fd27746eecc09bfabcaa
mqrtln <mart0l@outlook.com>
2023-01-24 22:39:04 +0100

    adding shit to RegexPartB and all tests run green ✔

--- src/main/java/Ex01/RegexPartB.java
+++ src/main/java/Ex01/RegexPartB.java
@@ -4,15 +4,15 @@ import java.util.ArrayList;
 
 public class RegexPartB {
 
-    public static String regexPartB(ArrayList<String> list){
+    public static String regexPartB(String list){
+        String regA = "(^@\\w+:).+(programming|programmering|[P|p][Gg][a-zA-Z0-9]{4}).+(exam|eksamen).+(20[0-9]{2}-[0-9]{2}-[0-9]|[0-9]{2}-[0-9]{2}-[0-9]{2})(.+)?([^?])$";
+        String regB = "(^@\\w+:).+(exam|eksamen).+(programming|programmering|[P|p][Gg][a-zA-Z0-9]{4}).+(20[0-9]{2}-[0-9]{2}-[0-9]|[0-9]{2}-[0-9]{2}-[0-9]{2})(.+)?([^?])$";
 
-        for(String line : list){
-            if(line.matches("(^@\\w+:).+(programming|programmering|[P|p][Gg][a-zA-Z0-9]{4}).+(exam|eksamen).+(20[0-9]{2}-[0-9]{2}-[0-9]|[0-9]{2}-[0-9]{2}-[0-9]{2})(.+)?([^?])$")){
-                return line;
-            } else if(line.matches("(^@\\w+:).+(exam|eksamen).+(programming|programmering|[P|p][Gg][a-zA-Z0-9]{4}).+(20[0-9]{2}-[0-9]{2}-[0-9]|[0-9]{2}-[0-9]{2}-[0-9]{2})(.+)?([^?])$")){
-                return line;
+        if(list.matches(regA)){
+                return regA;
+            } else if(list.matches(regB)){
+                return regB;
             }
-        }
         return "";
     }
 }
--- src/test/java/Ex01/TestShouldPassRegexPartB.java
+++ src/test/java/Ex01/TestShouldPassRegexPartB.java
@@ -4,30 +4,85 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class TestShouldPassRegexPartB {
 
 
-    ArrayList<String> input = new ArrayList<>();
+    @Test
+    public void shouldPassRegexPartB1(){
+        String input = "@Bogdan: The PG4200 re-exam is on 2022-08-05";
+        String regex = RegexPartB.regexPartB(input);
+        assertTrue(input.matches(regex));
+    }
+
+    @Test
+    public void shouldPassRegexPartB2(){
+        String input = "@Bogdan: Sample exercises for the PGR112 exam on 05-08-22 are on the repo.";
+        String regex = RegexPartB.regexPartB(input);
+        assertTrue(input.matches(regex));
+    }
+
+    @Test
+    public void shouldPassRegexPartB3(){
+        String input = "@Sven: The exam for PG4200 has been set for 22-08-05!";
+        String regex = RegexPartB.regexPartB(input);
+        assertTrue(input.matches(regex));
+    }
+
+    @Test
+    public void shouldPassRegexPartB4(){
+        String input = "@Harald: The programmering eksamen end on 2022-08-06.";
+        String regex = RegexPartB.regexPartB(input);
+        assertTrue(input.matches(regex));
+    }
+
+    @Test
+    public void shouldNotPassRegexPartB1(){
+        String input = "@Bogdan: The PG4200 is on 2022-08-05";
+        String regex = RegexPartB.regexPartB(input);
+        assertFalse(input.matches(regex));
+    }
 
 
     @Test
-    public void shouldPassRegexPartB1(){
-        input.add("@Bogdan:");
-        input.add("The");
-        input.add("PG4200");
-        input.add("re-exam");
-        input.add("is");
-        input.add("on");
-        input.add("05-08-22");
-        input.add("are");
-        input.add("on");
-        input.add("the");
-        input.add("repo.");
-
+    public void shouldNotPassRegexPartB2(){
+        String input = "@Harald Where can I find more exercises for the programmering eksamen?";
         String regex = RegexPartB.regexPartB(input);
-
-        assertTrue(input.toString().matches(regex));
+        assertFalse(input.matches(regex));
     }
+
+
+    @Test
+    public void shouldNotPassRegexPartB3(){
+        String input = "@Hardrada: Where can I find more ships to invade England?";
+        String regex = RegexPartB.regexPartB(input);
+        assertFalse(input.matches(regex));
+    }
+
+
+    @Test
+    public void shouldNotPassRegexPartB4(){
+        String input = "@Napoleon: Is it too late to learn what programmering means?";
+        String regex = RegexPartB.regexPartB(input);
+        assertFalse(input.matches(regex));
+    }
+
+
+    @Test
+    public void shouldNotPassRegexPartB5(){
+        String input = "@Armfeld: I plan to travel to get to Trondheim by 1718-12-31, for the HIS4230 exam!";
+        String regex = RegexPartB.regexPartB(input);
+        assertFalse(input.matches(regex));
+    }
+
+
+    @Test
+    public void shouldNotPassRegexPartB6(){
+        String input = "@Harald: I am ready for any programming exam";
+        String regex = RegexPartB.regexPartB(input);
+        assertFalse(input.matches(regex));
+    }
+
 }
